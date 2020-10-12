79edd2cedc068119f56be87d39c71660ffb4fdd0
Arthur Danskin <arthurdanskin@gmail.com>
2020-10-11 23:20:01 -0400

    ...

---
+++ sublime/up.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$1" = "pull" ]; then
+    cp -v User/* /cygdrive/c/Users/Arthur/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+elif [ "$1" = "push" ]; then
+    cp -v /cygdrive/c/Users/Arthur/AppData/Roaming/Sublime\ Text\ 3/Packages/User/* User/
+else
+    echo "push or pull"
+fi
+
