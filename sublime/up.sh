129cc674399ca9338095136d35ca12d7c7972a8b
Arthur Danskin <arthurdanskin@gmail.com>
2020-11-02 11:44:19 -0500

    add DEFINE_ENUM syntax

--- sublime/up.sh
+++ sublime/up.sh
@@ -4,8 +4,11 @@ git $1
 
 if [ "$1" = "pull" ]; then
     cp -v User/* /cygdrive/c/Users/$USER/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+    mkdir -p /cygdrive/c/Users/$USER/AppData/Roaming/Sublime\ Text\ 3/Packages/C++/
+    cp -v C++/* /cygdrive/c/Users/$USER/AppData/Roaming/Sublime\ Text\ 3/Packages/C++/
 elif [ "$1" = "push" ]; then
     cp -v /cygdrive/c/Users/$USER/AppData/Roaming/Sublime\ Text\ 3/Packages/User/* User/
+    cp -v /cygdrive/c/Users/$USER/AppData/Roaming/Sublime\ Text\ 3/Packages/C++/* C++/
 else
     echo "push or pull"
 fi
